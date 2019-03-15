bb427e124af24ab6102f99041e5a9f7cc9c29db2
MagnusIIIBR <nathan.mauclene@gmail.com>
2019-03-15 17:48:53 -0300

    Add files via upload
    - Implementação da geração de Séries
    - Implementação da geração de Episódios de Séries
    - Padronização dos alerts

--- js/index.js
+++ js/index.js
@@ -23,6 +23,8 @@ function bindButtons() {
 
         switch (selectedTipoGerador) {
             case "1":
+            case "2":
+            case "3":
                 $('#divFilmeSerie').show();
                 break;
 
@@ -33,22 +35,76 @@ function bindButtons() {
     });
 
     $('#btnLoadImdb').click(function () {
-        var txtImdbId = $("#txtImdbId").val();
-        if (txtImdbId.trim().length == 0) {
-            alert("O código do filme no IMDB é obrigatório");
+        var isFilme = false;
+        var isSerie = false;
+        var isEpisodioSerie = false;
+
+        var txtImdbId = $("#txtImdbId");
+        if (txtImdbId.val().trim().length == 0) {
+            bulmaToast.toast({ message: "O código do filme no IMDB é obrigatório" , type: "is-danger", duration: 4000});
             return false;
         }
 
         $('#pageLoader').addClass("is-active");
 
-        dataFromOMDB = loadFromOMDB(txtImdbId);
-        dataFromTMDB = loadFromTMDB(txtImdbId);
-       
+        dataFromOMDB = loadFromOMDB(txtImdbId.val());
+        dataFromTMDB = loadFromTMDB(txtImdbId.val());
+
+        if (dataFromTMDB.movie_results.length > 0) isFilme = true;
+        if (dataFromTMDB.tv_results.length > 0) isSerie = true;
+        if (dataFromTMDB.tv_episode_results.length > 0) isEpisodioSerie = true;
+
+        if (selectedTipoGerador == "1" && !isFilme) {
+            if (isSerie) {
+                bulmaToast.toast({ message: "O código IMDB usado pertence ao tipo 'Série de TV', mas relaxa, já arrumei pra você!" , type: "is-alert", duration: 4000});
+                selectedTipoGerador = "2";
+                $('#cmbTipoGerador').val("2");
+            }
+            
+            if (isEpisodioSerie) {
+                bulmaToast.toast({ message: "O código IMDB usado pertence ao tipo 'Episódio de série de TV', mas relaxa, já arrumei pra você!"  , type: "is-alert", duration: 4000});
+                selectedTipoGerador = "3";
+                $('#cmbTipoGerador').val("3");
+            }
+        }
+
+        if (selectedTipoGerador == "2" && !isSerie) {
+            if (isFilme) {
+                bulmaToast.toast({ message: "O código IMDB usado pertence ao tipo 'Filme', mas relaxa, já arrumei pra você!" , type: "is-alert", duration: 4000 });
+                selectedTipoGerador = "1";
+                $('#cmbTipoGerador').val("1");
+            }
+            
+            if (isEpisodioSerie) {
+                bulmaToast.toast({ message: "O código IMDB usado pertence ao tipo 'Episódio de série de TV', mas relaxa, já arrumei pra você!"  , type: "is-alert", duration: 4000});
+                selectedTipoGerador = "3";
+                $('#cmbTipoGerador').val("3");
+            }
+        }
+
+        
+        if (selectedTipoGerador == "3" && !isEpisodioSerie) {
+            if (isFilme) {
+                bulmaToast.toast({ message: "O código IMDB usado pertence ao tipo 'Filme', mas relaxa, já arrumei pra você!"  , type: "is-alert", duration: 4000});
+                selectedTipoGerador = "1";
+                $('#cmbTipoGerador').val("1");
+            }
+            
+            if (isSerie) {
+                bulmaToast.toast({ message: "O código IMDB usado pertence ao tipo 'Série de TV', mas relaxa, já arrumei pra você!" , type: "is-alert", duration: 4000 });
+                selectedTipoGerador = "2";
+                $('#cmbTipoGerador').val("2");
+            }
+        }
+
+        console.log(dataFromOMDB);
+        console.log(dataFromTMDB);
+
         $('#pageLoader').removeClass("is-active");
 
         $('#btnGerar').show();
 
-        bulmaToast.toast({ message: "Pronto! Basta clicar em Gerar BBCode!" });
+        bulmaToast.toast({ message: "Pronto! Basta clicar em Gerar BBCode!"  , type: "is-success", duration: 4000});
 
     });
 
@@ -56,45 +112,7 @@ function bindButtons() {
     $('#btnGerar').click(function () {
 
         var BBCode = "";
-
-        switch (selectedTipoGerador) {
-            case "1":
-
-                BBCode += "[b][align=center]" + formatImageBBCodeWithUrl(urlSeparatorImage) + "[/align][/b]";
-                BBCode += addBreakLines();
-                BBCode += "[b][align=center]" + dataFromTMDB.movie_results[0].title + " (" + dataFromOMDB.Title + ")" + "[/align][/b]";
-                BBCode += "[b][align=center] (" + dataFromOMDB.Year + ") [/align][/b]";
-
-                BBCode += "[b][align=center]" + formatImageBBCodeWithUrl(urlSeparatorImage) + "[/align][/b]";
-                BBCode += addBreakLines();
-
-                BBCode += "[align=center]" + formatImageBBCodeWithUrl(baseUrlTMDbPoster + dataFromTMDB.movie_results[0].poster_path) + "[/align]";
-
-                BBCode += addBreakLines();
-                BBCode += addBreakLines();
-                BBCode += addBreakLines();
-                BBCode += formatDescricaoBBCode();
-
-                BBCode += addBreakLines();
-                BBCode += addBreakLines();
-                BBCode += addBreakLines();
-                BBCode += formatInformacoesBBCode();
-
-                BBCode += addBreakLines();
-                BBCode += addBreakLines();
-                BBCode += addBreakLines();
-                BBCode += formatElencoBBCode();
-
-                BBCode += addBreakLines();
-                BBCode += addBreakLines();
-                BBCode += addBreakLines();
-                BBCode += formatCriticasBBCode();
-
-                break;
-
-            default:
-                break;
-        }
+        BBCode = gerarBBCodeSerieFilmes();
 
         $('#txtBBCode').val(BBCode);
 
@@ -102,16 +120,90 @@ function bindButtons() {
 
         $('#divBBCode').show();
 
-        bulmaToast.toast({ message: "BBCode gerado! Basta copiar para sua postagem!" });
+        bulmaToast.toast({ message: "BBCode gerado! Basta copiar para sua postagem!" , type: "is-success", duration: 4000});
     });
 };
 
+var gerarBBCodeSerieFilmes = function () {
+    var BBCode = "";
+
+    BBCode += "[b][align=center]" + formatImageBBCodeWithUrl(urlSeparatorImage) + "[/align][/b]";
+    BBCode += addBreakLines();
+
+    switch (selectedTipoGerador) {
+        case "1":
+            BBCode += "[b][align=center]" + dataFromTMDB.movie_results[0].title + " (" + dataFromOMDB.Title + ")" + "[/align][/b]";
+            break;
+        case "2":
+            BBCode += "[b][align=center]" + dataFromTMDB.tv_results[0].name + " (" + dataFromOMDB.Title + ")" + "[/align][/b]";
+            break;
+        case "3":
+            BBCode += "[b][align=center]" + dataFromTMDB.tv_episode_results[0].name + " (" + dataFromOMDB.Title + ")" + "[/align][/b]";
+            break;
+
+        default:
+            break;
+    }
+    BBCode += "[b][align=center] (-- " + dataFromOMDB.Year + " --) [/align][/b]";
+
+    BBCode += "[b][align=center]" + formatImageBBCodeWithUrl(urlSeparatorImage) + "[/align][/b]";
+    BBCode += addBreakLines();
+
+    switch (selectedTipoGerador) {
+        case "1":
+            BBCode += "[align=center]" + formatImageBBCodeWithUrl(baseUrlTMDbPoster + dataFromTMDB.movie_results[0].poster_path) + "[/align]";
+            break;
+        case "2":
+            BBCode += "[align=center]" + formatImageBBCodeWithUrl(baseUrlTMDbPoster + dataFromTMDB.tv_results[0].poster_path) + "[/align]";
+            break;
+        case "3":
+            BBCode += "[align=center]" + formatImageBBCodeWithUrl(baseUrlTMDbPoster + dataFromTMDB.tv_episode_results[0].still_path) + "[/align]";
+            break;
+
+        default:
+            break;
+    }
+
+    BBCode += addBreakLines();
+    BBCode += addBreakLines();
+    BBCode += addBreakLines();
+    BBCode += formatDescricaoBBCode();
+
+    BBCode += addBreakLines();
+    BBCode += addBreakLines();
+    BBCode += addBreakLines();
+    BBCode += formatInformacoesBBCode();
+
+    BBCode += addBreakLines();
+    BBCode += addBreakLines();
+    BBCode += addBreakLines();
+    BBCode += formatElencoBBCode();
+
+    BBCode += addBreakLines();
+    BBCode += addBreakLines();
+    BBCode += addBreakLines();
+    BBCode += formatCriticasBBCode();
+
+    return BBCode;
+};
 
 var formatDescricaoBBCode = function () {
     var str = "";
     str += "[b][align=center]" + formatImageBBCodeWithUrl(urlSeparatorImage) + "[/align][/b]";
     str += "\n[b][align=center]SINOPSE[/align][/b]";
-    str += "\n[align=center]" + dataFromTMDB.movie_results[0].overview + "[/align]";
+    switch (selectedTipoGerador) {
+        case "1":
+            str += "\n[align=center]" + dataFromTMDB.movie_results[0].overview + "[/align]";
+            break;
+        case "2":
+            str += "\n[align=center]" + dataFromTMDB.tv_results[0].overview + "[/align]";
+            break;
+        case "3":
+            str += "\n[align=center]" + dataFromTMDB.tv_episode_results[0].overview + "[/align]";
+            break;
+        default:
+            break;
+    }
 
     return str;
 }
@@ -120,15 +212,39 @@ var formatInformacoesBBCode = function () {
     var str = "";
     str += "[b][align=center]" + formatImageBBCodeWithUrl(urlSeparatorImage) + "[/align][/b]";
     str += "\n[b][align=center]INFORMAÇÕES[/align][/b]";
-    str += "\n[align=center]Data de Lançamento: " + dataFromOMDB.Released + "[/align]";
-    str += "\n[align=center]Tempo de Filme: " + dataFromOMDB.Runtime + "[/align]";
-    str += "\n[align=center]Produtora: " + dataFromOMDB.Production + "[/align]";
+
+    switch (selectedTipoGerador) {
+        case "1":
+        case "2":
+            str += "\n[align=center]Data de Lançamento: " + dataFromOMDB.Released + "[/align]";
+            break;
+
+        case "3":
+            str += "\n[align=center]Data de Lançamento: " + reformatDate(dataFromTMDB.tv_episode_results[0].air_date) + "[/align]";
+            str += "\n[align=center]Temporada: " + dataFromOMDB.Season + "[/align]";
+            str += "\n[align=center]Episódio: " + dataFromOMDB.Episode + "[/align]";
+            break;
+        default:
+            break;
+    }
+
+
+    str += "\n[align=center]Tempo: " + dataFromOMDB.Runtime + "[/align]";
+    if (selectedTipoGerador == "1") {
+        str += "\n[align=center]Produtora: " + dataFromOMDB.Production + "[/align]";
+    }
     str += "\n[align=center]País de Origem: " + dataFromOMDB.Country + "[/align]";
     str += "\n[align=center]Gêneros: " + dataFromOMDB.Genre + "[/align]";
     str += "\n[align=center]Site: [url=" + dataFromOMDB.Website + "] Clique aqui [/url][/align]";
 
     return str;
 }
+
+function reformatDate(dateStr) {
+    dArr = dateStr.split("-");
+    return dArr[2] + "/" + dArr[1] + "/" + dArr[0].substring(2);
+}
+
 
 var formatElencoBBCode = function () {
     var str = "";
@@ -137,14 +253,16 @@ var formatElencoBBCode = function () {
     var arr = dataFromOMDB.Actors.split(',');
 
     arr.forEach(element => {
-        str += "\n[align=center]" + element.trim() + " - ATOR(IZ)[/align]"
+        str += "\n[align=center]" + element.trim() + " - ATOR/ATRIZ[/align]"
     });
 
-    arr = dataFromOMDB.Director.split(',');
+    if (dataFromOMDB.Director != "N/A") {
+        arr = dataFromOMDB.Director.split(',');
 
-    arr.forEach(element => {
-        str += "\n[align=center]" + element.trim() + " - DIRETOR(A)[/align]"
-    });
+        arr.forEach(element => {
+            str += "\n[align=center]" + element.trim() + " - DIRETOR(A)[/align]"
+        });
+    }
 
     return str;
 }
@@ -160,35 +278,37 @@ var addBreakLines = function () {
 
 var formatCriticasBBCode = function () {
     var str = "";
-    str += "[align=center]" + formatImageBBCodeWithUrl(urlSeparatorImage) + "[/align]";
-    str += "\n[b][align=center]CRÍTICA[/align][/b]";
 
-    dataFromOMDB.Ratings.forEach(element => {
-        switch (element.Source) {
-            case "Internet Movie Database":
-                str += "\n[align=center]" + formatImageBBCodeWithUrl(urlIMDBLogoImage) + "[/align]";
-                str += "\n[align=center][b][url=https://www.imdb.com/title/" + dataFromOMDB.imdbID + "/]" + element.Value.trim() + "[/b][/url][/align]"
-                break;
+    if (dataFromOMDB.Ratings.length > 0) {
+        str += "[align=center]" + formatImageBBCodeWithUrl(urlSeparatorImage) + "[/align]";
+        str += "\n[b][align=center]CRÍTICA[/align][/b]";
 
-            case "Rotten Tomatoes":
-                str += "\n[align=center]" + formatImageBBCodeWithUrl(urlRottenTomatoesLogoImage) + "[/align]";
-                str += "\n[align=center][b]" + element.Value.trim() + "[/b][/align]"
+        dataFromOMDB.Ratings.forEach(element => {
+            switch (element.Source) {
+                case "Internet Movie Database":
+                    str += "\n[align=center]" + formatImageBBCodeWithUrl(urlIMDBLogoImage) + "[/align]";
+                    str += "\n[align=center][b][url=https://www.imdb.com/title/" + dataFromOMDB.imdbID + "/]" + element.Value.trim() + "[/b][/url][/align]"
+                    break;
 
-                break;
+                case "Rotten Tomatoes":
+                    str += "\n[align=center]" + formatImageBBCodeWithUrl(urlRottenTomatoesLogoImage) + "[/align]";
+                    str += "\n[align=center][b]" + element.Value.trim() + "[/b][/align]"
 
-            case "Metacritic":
-                str += "\n[align=center]" + formatImageBBCodeWithUrl(urlMetacriticLogoImage) + "[/align]";
-                str += "\n[align=center][b]" + element.Value.trim() + "[/b][/align]"
-                break;
-            default:
-                break;
-        }
+                    break;
+
+                case "Metacritic":
+                    str += "\n[align=center]" + formatImageBBCodeWithUrl(urlMetacriticLogoImage) + "[/align]";
+                    str += "\n[align=center][b]" + element.Value.trim() + "[/b][/align]"
+                    break;
+                default:
+                    break;
+            }
 
 
 
+        });
 
-    });
-
+    }
 
     return str;
 }
